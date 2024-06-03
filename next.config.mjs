4f3748156635280dc5f96588d7cb6ad28eb9b6ab
henok10 <56106209+henok10@users.noreply.github.com>
2024-06-03 10:58:46 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   basePath: "/my-portfolio",
   assetPrefix: "/my-portfolio/",
   output: "export",
 };
-=======
-    basePath: '/my-portfolio',
-    assetPrefix: '/my-portfolio',
-  };
-  
->>>>>>> 7547d91cc5b0ded846f9af009743983653c2e03a
 
 export default nextConfig;
